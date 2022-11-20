a205e15c46f70da3b61316baa0e2adc68ad7f36b
Tategami99 <k07bautista@gmail.com>
2022-11-19 16:48:11 -0800

    Revert "revert"
    This reverts commit 65fd6262340ff7e9fb61604d1236c3dd6427ccb8.

---
+++ GameScene.js
@@ -0,0 +1,15 @@
+import Phaser from "phaser";
+
+export default class GameScen extends Phaser.Scene{
+    constructor(){
+        super('gameScene')
+    }
+
+    preload(){
+        
+    }
+
+    create(){
+
+    }
+}
